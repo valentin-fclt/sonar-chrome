4d3eea77cacafaf5a0df33cd4a51cd30edfc1783
Valentin Foucault <valentin@valentinfoucault.com>
2023-09-01 19:09:37 +0200

    Update README.md and the SONAR_API_URL constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const SONAR_API_URL = 'http://localhost:3001';
+export const SONAR_API_URL = 'https://api.sonarpro.app';
 export const FAQ_URL =
   'https://coffee-yumberry-a8b.notion.site/Sonar-Chrome-Extension-FAQ-2dc6ccc468ce4118b68f6711868fec66';
 export const GOOGLE_SIGNIN_URL =
